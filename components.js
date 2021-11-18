e155bfc341bdb4b93942a8ddb031b9f31dac2911
sairaj mote <sairajmote3@gmail.com>
2021-11-18 20:11:32 +0530

    Feature update and bug fixes
    - Added article title and heading renaming in-place for sub-admins
    - fixed bug for version history
    - version history UX improvements

--- components.js
+++ components.js
@@ -3428,3 +3428,233 @@ customElements.define('sm-switch', class extends HTMLElement {
     }
 
 })
+
+const textField = document.createElement('template')
+textField.innerHTML = `
+<style>
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    :host{
+        --accent-color: #4d2588;
+        --text-color: 17, 17, 17;
+        --background-color: 255, 255, 255;
+    }
+    .text-field{
+        display: flex;
+        align-items: center;
+    }
+    .text{
+        padding: 0.6rem 0;
+        transition: background-color 0.3s;
+        border-bottom: 0.15rem solid transparent;
+        overflow-wrap: break-word;
+        word-wrap: break-word;
+        word-break: break-all;
+        word-break: break-word;
+        -moz-hyphens: auto;
+        -webkit-hyphens: auto;
+        hyphens: auto;
+    }
+    .text:focus{
+        outline: none;
+        border-bottom: 0.15rem solid var(--accent-color);
+    }
+    .text:focus-visible{
+        outline: none;
+        background: solid rgba(var(--text-color), 0.06);
+    }
+    .editable{
+        border-bottom: 0.15rem solid rgba(var(--text-color), 0.6);
+    }
+    .icon-container{
+        display: grid;
+        position: relative;
+        margin-left: 0.5rem;
+    }
+    :host([disabled]) .icon-container{
+        display: none;
+    }
+    .icon{
+        grid-area: 1/-1;
+        cursor: pointer;
+        height: 1.2rem;
+        width: 1.2rem;
+        fill: rgba(var(--text-color), 1);
+    }
+    .hide{
+        display: none;
+    }
+</style>
+<div class="text-field">
+    <div class="text" part="text"></div>
+    <div tabindex="0" class="icon-container">
+        <svg class="edit-button icon" title="edit" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
+        <svg class="save-button icon hide" title="Save" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>
+    </div>
+</div>
+`
+
+customElements.define('text-field', class extends HTMLElement {
+    constructor() {
+        super()
+        this.attachShadow({
+            mode: 'open'
+        }).append(textField.content.cloneNode(true))
+
+        this.textField = this.shadowRoot.querySelector('.text-field')
+        this.textContainer = this.textField.children[0]
+        this.iconsContainer = this.textField.children[1]
+        this.editButton = this.textField.querySelector('.edit-button')
+        this.saveButton = this.textField.querySelector('.save-button')
+        this.isTextEditable = false
+        this.isDisabled = false
+
+        this.fireEvent = this.fireEvent.bind(this)
+        this.setEditable = this.setEditable.bind(this)
+        this.setNonEditable = this.setNonEditable.bind(this)
+        this.revert = this.revert.bind(this)
+    }
+
+    static get observedAttributes() {
+        return ['disabled', 'value']
+    }
+
+    get value() {
+        return this.text
+    }
+    set value(val) {
+        this.setAttribute('value', val)
+    }
+    set disabled(val) {
+        this.isDisabled = val
+        if (this.isDisabled)
+            this.setAttribute('disabled', '')
+        else
+            this.removeAttribute('disabled')
+    }
+    fireEvent(value) {
+        let event = new CustomEvent('change', {
+            bubbles: true,
+            cancelable: true,
+            composed: true,
+            detail: {
+                value
+            }
+        });
+        this.dispatchEvent(event);
+    }
+
+    setEditable() {
+        if (this.isTextEditable) return
+        this.textContainer.contentEditable = true
+        this.textContainer.classList.add('editable')
+        this.textContainer.focus()
+        document.execCommand('selectAll', false, null);
+        this.editButton.animate(this.rotateOut, this.animOptions).onfinish = () => {
+            this.editButton.classList.add('hide')
+        }
+        setTimeout(() => {
+            this.saveButton.classList.remove('hide')
+            this.saveButton.animate(this.rotateIn, this.animOptions)
+        }, 100);
+        this.isTextEditable = true
+    }
+    setNonEditable() {
+        if (!this.isTextEditable) return
+        this.textContainer.contentEditable = false
+        this.textContainer.classList.remove('editable')
+        const newValue = this.textContainer.textContent.trim()
+        if (this.text !== newValue && newValue !== '') {
+            this.setAttribute('value', this.textContainer.textContent)
+            this.text = this.textContainer.textContent.trim()
+            this.fireEvent(this.text)
+        } else {
+            this.value = this.text
+        }
+        this.saveButton.animate(this.rotateOut, this.animOptions).onfinish = () => {
+            this.saveButton.classList.add('hide')
+        }
+        setTimeout(() => {
+            this.editButton.classList.remove('hide')
+            this.editButton.animate(this.rotateIn, this.animOptions)
+        }, 100);
+        this.isTextEditable = false
+    }
+
+    revert() {
+        if (this.textContainer.isContentEditable) {
+            this.value = this.text
+            this.setNonEditable()
+        }
+    }
+
+    connectedCallback() {
+        this.text
+        if (this.hasAttribute('value')) {
+            this.text = this.getAttribute('value')
+            this.textContainer.textContent = this.text
+        }
+        if (this.hasAttribute('disabled'))
+            this.isDisabled = true
+        else
+            this.isDisabled = false
+
+        this.rotateOut = [
+            {
+                transform: 'rotate(0)',
+                opacity: 1
+            },
+            {
+                transform: 'rotate(90deg)',
+                opacity: 0
+            },
+        ]
+        this.rotateIn = [
+            {
+                transform: 'rotate(-90deg)',
+                opacity: 0
+            },
+            {
+                transform: 'rotate(0)',
+                opacity: 1
+            },
+        ]
+        this.animOptions = {
+            duration: 300,
+            easing: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+            fill: 'forwards'
+        }
+        if (!this.isDisabled) {
+            this.iconsContainer.classList.remove('hide')
+            this.textContainer.addEventListener('dblclick', this.setEditable)
+            this.editButton.addEventListener('click', this.setEditable)
+            this.saveButton.addEventListener('click', this.setNonEditable)
+        }
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'disabled') {
+            if (this.hasAttribute('disabled')) {
+                this.textContainer.removeEventListener('dblclick', this.setEditable)
+                this.editButton.removeEventListener('click', this.setEditable)
+                this.saveButton.removeEventListener('click', this.setNonEditable)
+                this.revert()
+            }
+            else {
+                this.textContainer.addEventListener('dblclick', this.setEditable)
+                this.editButton.addEventListener('click', this.setEditable)
+                this.saveButton.addEventListener('click', this.setNonEditable)
+            }
+        } else if (name === 'value') {
+            this.text = newValue
+            this.textContainer.textContent = newValue
+        }
+    }
+    disconnectedCallback() {
+        this.textContainer.removeEventListener('dblclick', this.setEditable)
+        this.editButton.removeEventListener('click', this.setEditable)
+        this.saveButton.removeEventListener('click', this.setNonEditable)
+    }
+})
